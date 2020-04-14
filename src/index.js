716a8378505b619c27248cdd1c30a72492a87d08
Artur Saliyev <artursaliyev>
2020-04-14 18:38:48 +0500

    Set up redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./app/App";
+import store from "./configureStore";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"));

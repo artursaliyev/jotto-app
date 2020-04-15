9f8e4d2c630ed2f050413975743769a6eabd0e20
Artur Saliyev <artursaliyev>
2020-04-15 22:41:10 +0500

    add getSecretWord action creator on App mount

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./App.css";
 
 import GuessedWords from "../cuessed-words"
 import Congrats from "../congrats"
+import { getSecretWord } from "../actions"
 
-function App() {
-  return <div className="container">
-    <h1>Jotto App</h1>
-    <Congrats success={true} />
-    <GuessedWords guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]} />
-  </div>;
+export class UnconnectedApp extends Component {
+
+  componentDidMount() {
+    this.props.getSecretWord();
+  }
+
+  render() {
+    return <div className="container">
+      <h1>Jotto App  </h1>
+      <Congrats success={this.props.success} />
+      <GuessedWords guessedWords={this.props.guessedWords} />
+    </div >;
+  }
 }
 
-export default App;
+
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+}
+export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);

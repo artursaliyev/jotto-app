0e91ef86a33b671fa27d735dddb4c16c9dbf6d34
Artur Saliyev <artursaliyev>
2020-04-15 14:53:24 +0500

    set up guessWord and tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 export const actionTypes = {
     CORRECT_GUESS: "CORRECT_GUESS",
+    GUESS_WORD: "GUESS_WORD",
 }
 
-/**
- * @function correctGuest
- * @returns {object} - Action onject with type `CORRECT_GUESS`
- */
-export function correctGuess() {
-    return { type: actionTypes.CORRECT_GUESS }
+
+export const guessWord = (guessedWord) => {
+    return function (dispatch, getState) {
+
+    }
 }

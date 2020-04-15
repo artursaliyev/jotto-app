ac77316eb5dc14d0a23462db3ae95cdf82746667
Artur Saliyev <artursaliyev>
2020-04-15 18:13:45 +0500

    add redux props tests to input

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -23,7 +23,7 @@ describe("getSecretWord action creator", () => {
             });
         });
 
-        return store.dispatch(getSecretWord())
+        store.dispatch(getSecretWord())
             .then(() => {
                 const newState = store.getState();
                 expect(newState.secretWord).toBe(secretWord);
